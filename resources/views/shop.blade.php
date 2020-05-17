d354a4fa0cb172b92d59010eeffac2d510924b8e
juste1599 <65291588+juste1599@users.noreply.github.com>
2020-05-17 17:40:00 +0300

    meniu 1

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -14,8 +14,12 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
-    <style>
 
+    <style>
+    .meniu
+    {
+        padding-left: 225px;
+    }
 
 
     </style>
@@ -45,16 +49,18 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Logo</a>
+
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
+            <div class="meniu">
+            <ul class="nav navbar-nav" style="padding-left: 225px">
+                <li class="active" style="padding: 100px"><a href="#">Home</a></li>
                 <li><a href="#">Products</a></li>
                 <li><a href="#">Deals</a></li>
                 <li><a href="#">Stores</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
+            </div>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#"><span class="glyphicon glyphicon-user"></span> Your Account</a></li>
                 <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
