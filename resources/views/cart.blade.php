0979d97217c263eec961ed3554a87791f99c2b10
juste1599 <65291588+juste1599@users.noreply.github.com>
2020-05-18 22:20:22 +0300

    cart test1

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,81 +1,65 @@
 @extends('layouts.app')
 <style>
-
+    @media (max-width: 1200px) {
+        #cart_sonas {
+        height: auto;
+            width: auto;
+        }
+    }
 </style>
 @section('turinys')
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 
-    <!-- Cart -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-    <div class="cart_section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-10 offset-lg-1">
-                    <div class="cart_container">
-                        <div class="cart_title">Cart</div>
-                        <div class="cart_items">
-                            <ul class="cart_list">
-                                <li class="cart_item clearfix">
-                                   {{-- @foreach($results as $result)
-                                        <div class="cart_item_info d-flex flex-md-row flex-column justify-content-between">
-                                            <div class="cart_item_name cart_info_col">
-                                                <div class="cart_item_title"></div>
-                                                <div class="cart_item_text">
-                                                    <span class="glyphicon glyphicon-barcode" style="height: 50px;width: 50px;"></span>
---}}{{--                                                    <img src="images/preke.png" alt="" height="50px" width="50px"></div>--}}{{--
-                                            </div>
-                                            <div class="cart_item_name cart_info_col">
-                                                <div class="cart_item_title">Prekes kodas</div>
-                                                <div class="cart_item_text">{{ $result->id_Preke }}</div>
-                                            </div>
-                                            <div class="cart_item_color cart_info_col">
-                                                <div class="cart_item_title">Pavadinimas</div>
-                                                <div class="cart_item_text">{{ $result->Pavadinimas }}</div>
-                                            </div>
-                                            <div class="cart_item_quantity cart_info_col">
-                                                <div class="cart_item_title">Kaina</div>
-                                            </div>
-                                            <div class="cart_item_price cart_info_col">
-                                                <div class="cart_item_title">Kiekis</div>
-                                                <div class="cart_item_text">{{ $result->kiekis }}</div>
-                                            </div>
-                                            <div class="cart_item_price cart_info_col">
-                                                <div class="cart_item_title">Spalva</div>
-                                                <div class="cart_item_text">{{ $result->Spalva }}</div>
-                                            </div>
-                                            <div class="cart_item_price cart_info_col">
-                                                <div class="cart_item_title"></div>
-                                                <div class="cart_item_text">
-                                                    <a href="{{ route('cart.edit',['id'=>$result->id_Tarpine])  }}">
-                                                      --}}{{--  <img src="{{asset('/images/iks.png')}}" height="20px" width="20px" >--}}{{--
-                                                        <span class="glyphicon glyphicon-remove" style="height: 20px;width: 20px"></span>
-                                                    </a>
-                                                </div>
+    <div class="container" id="cart_sonas">
+        <div class="col-lg-10 offset-lg-1">
 
-                                            </div>
-                                        </div>
-                                    @endforeach--}}
-                                </li>
-                            </ul>
-                        </div>
-
-                        <!-- Order Total -->
-                        <div class="order_total">
-                            <div class="order_total_content text-md-right">
-                               {{-- @foreach($suma as $sumele)
-                                    <div class="order_total_title">Order total:</div>
-                                    <div class="order_total_amount">{{ $sumele->Final_Kaina }} Eur</div>
-                                    {{session(['kaina'=>$sumele->Final_Kaina])}}
-                                @endforeach--}}
-                            </div>
-                        </div>
-
-                        <div class="cart_buttons">
-                            <button type="button" class="button cart_button_checkout"><a href="/order">Order</a></button>
+        <table id="cart" class="table table-hover table-condensed">
+            <thead>
+            <tr>
+                <th style="width:50%">Product</th>
+                <th style="width:10%">Price</th>
+                <th style="width:8%">Quantity</th>
+                <th style="width:22%" class="text-center">Subtotal</th>
+                <th style="width:10%"></th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td data-th="Product">
+                    <div class="row">
+                        <div class="col-sm-4 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div>
+                        <div class="col-sm-4">
+                            <h4 class="nomargin">Product 1</h4>
+                            <p>Quis aute iure reprehenderit in voluptate </p>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
+                </td>
+                <td data-th="Price">$1.99</td>
+                <td data-th="Quantity">
+                    <input type="number" class="form-control text-center" value="1">
+                </td>
+                <td data-th="Subtotal" class="text-center">1.99</td>
+                <td class="actions" data-th="">
+                    <button class="btn btn-sm"><i class="glyphicon glyphicon-trash" style="color: red"></i></button>
+                </td>
+            </tr>
+            </tbody>
+            <tfoot>
+            <tr class="visible-xs">
+                <td class="text-center"><strong>Total 1.99</strong></td>
+            </tr>
+            <tr>
+                <td colspan="2" class="hidden-xs"></td>
+                <td class="hidden-xs text-center"><strong>Total $1.99</strong></td>
+                <td><a href="#" class="btn btn-block" style="background-color: #61892F; color: white">Order</a></td>
+            </tr>
+            </tfoot>
+        </table>
+    </div>
     </div>
 
 @endsection
