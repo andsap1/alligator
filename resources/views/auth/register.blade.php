7de5c6b782df7c96ca99624b656b654acb000390
juste1599 <65291588+juste1599@users.noreply.github.com>
2020-05-20 00:18:06 +0300

    update register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('turinys')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -63,8 +63,8 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-10 offset-md-4">
+                                <button type="submit" id="mygtukas"class="btn btn-primary">
                                     {{ __('Register') }}
                                 </button>
                             </div>
