5ad723ab7702fd6048e8116687f14c0724888e85
andsap1 <48027218+andsap1@users.noreply.github.com>
2020-05-20 22:30:29 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -124,21 +124,8 @@
 
             </ul>
         </div>
-
     </div>
-
 </nav>
-<div class="dropdown show">
-    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown link
-    </a>
-
-    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-        <a class="dropdown-item" href="#">Action</a>
-        <a class="dropdown-item" href="#">Another action</a>
-        <a class="dropdown-item" href="#">Something else here</a>
-    </div>
-</div>
 {{--   /*kai sumazini atsidengia kitoje vietoje side bar*/--}}
 <div class="container" id="sideNot"  media="(min-width: 766px)" style="margin-left: 40px;" >
     <button class="nav nav-sidebar" id="button1" type="button"  data-toggle="collapse" data-target="#navbarSide"
