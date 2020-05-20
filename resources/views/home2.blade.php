f443a2bef73422467564c36ee888ce8dfafe5d06
darstu <65291573+darstu@users.noreply.github.com>
2020-05-20 22:30:24 +0300

    sidebar back

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -1,41 +1,35 @@
 <section class="text-center">
     <div class="col-md-4">
-        <div class="card item">
-            <div class="info-wrap1">
-                <a href="/alligator/public/shop1" class="text-body">
-                    <i class="fa fa-tachometer fa-4x "></i></br>
-                    <h10>Visos prekės</h10>
-                    <div class="img-wrap">
-                        <img src="http://placehold.it/299x299">
-                </a>
+    <div class="card item">
+        <a href="/alligator/public/shop1" class="text-body">
+            <i class="fa fa-tachometer fa-4x "></i></br>
+            <h10>Visos prekės</h10>
+            <div class="img-wrap">
+                <img src="http://placehold.it/299x299">
             </div>
-        </div>
+        </a>
     </div>
     </div>
     <div class="col-md-4">
-        <div class="card item">
-            <div class="info-wrap1">
-                <a href="/alligator/public/shop1/1" class="text-body">
-                    <i class="fa fa-tachometer fa-4x "></i></br>
-                    <h10>Doors tools</h10>
-                    <div class="img-wrap">
-                        <img src="http://placehold.it/299x299">
-                </a>
+    <div class="card item">
+            <a href="/alligator/public/shop1/1" class="text-body">
+                <i class="fa fa-tachometer fa-4x "></i></br>
+                <h10>Doors tools</h10>
+            <div class="img-wrap">
+                <img src="http://placehold.it/299x299">
             </div>
-        </div>
+            </a>
     </div>
     </div>
     <div class="col-md-4">
-        <div class="card item">
-            <div class="info-wrap1">
-                <a href="/alligator/public/shop1/2" class="text-body">
-                    <i class="fa fa-tachometer fa-4x "></i></br>
-                    <h10>Whale tails</h10>
-                    <div class="img-wrap">
-                        <img src="http://placehold.it/299x299">
-                </a>
+    <div class="card item">
+        <a href="/alligator/public/shop1/2" class="text-body">
+            <i class="fa fa-tachometer fa-4x "></i></br>
+            <h10>Whale tails</h10>
+            <div class="img-wrap">
+                <img src="http://placehold.it/299x299">
             </div>
-        </div>
+        </a>
     </div>
     </div>
 </section>
