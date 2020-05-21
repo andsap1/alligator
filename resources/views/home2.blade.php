fc36706e926d528d3cede7ba26c93537f4451412
darstu <65291573+darstu@users.noreply.github.com>
2020-05-21 17:52:43 +0300

    asdad

--- resources/views/home2.blade.php
+++ resources/views/home2.blade.php
@@ -2,20 +2,8 @@
 
     <div class="col-md-4">
     <div class="card item">
-
-        <a href="/alligator/public/shop1" class="text-body">
-         </br>
-            <h10>Visos prekės</h10>
-            <div class="img-wrap">
-                <img src="http://placehold.it/299x299">
-            </div>
-        </a>
-    </div>
-    </div>
-    <div class="col-md-4">
-    <div class="card item">
             <a href="/alligator/public/shop1/1" class="text-body">
-                <i class="fa fa-tachometer fa-4x "></i></br>
+                </br>
                 <h10>Doors tools</h10>
             <div class="img-wrap">
                 <img src="{{ asset('/images/item1.jpg')}}">
